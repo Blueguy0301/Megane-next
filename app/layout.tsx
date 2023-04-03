f08a531863c4a8caf1a1f55fa0ff32546fd5c669
Blueguy0301 <60511119+Blueguy0301@users.noreply.github.com>
2023-04-03 21:20:16 +0800

    test add on serviceworker

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,24 @@
 "use client"
 import type { ReactNode } from "react"
+import { useEffect } from "react"
 import Head from "./Head"
 import "../styles/global.css"
 import { SessionProvider } from "next-auth/react"
+import type { Workbox } from "workbox-window"
 
+declare global {
+	interface Window {
+		workbox: Workbox
+	}
+}
 export default function RootLayout({ children }: { children: ReactNode }) {
+	useEffect(() => {
+		if ("serviceWorker" in navigator && window.workbox !== undefined) {
+			const wb = window.workbox
+			wb.register()
+		}
+	}, [])
+
 	return (
 		<html lang="en">
 			<Head title={"Megane"} />
